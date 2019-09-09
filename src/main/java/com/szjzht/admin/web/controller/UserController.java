dac877cc7a2b7554e943391c9b1f2c897f14cd62
MingwangLi <lmw78609@163.com>
2019-09-09 18:58:24 +0800

    上一步完美整合swagger之后 添加swagger注解规范

--- src/main/java/com/szjzht/admin/web/controller/UserController.java
+++ src/main/java/com/szjzht/admin/web/controller/UserController.java
@@ -3,6 +3,7 @@ package com.szjzht.admin.web.controller;
 import com.szjzht.admin.common.constant.Result;
 import com.szjzht.admin.mapper.UserMapper;
 import com.szjzht.admin.model.User;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,7 @@ import java.util.List;
  * @Date: 2019/9/9 14:27
  * @Description:
  */
+@Api(value = "/user", description = "用户模块")
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -26,14 +28,22 @@ public class UserController {
     private UserMapper userMapper;
 
     // TODO: 2019/9/9 这里的权限既可以通过数据库RoleResource配置 也可以通过@PreAuthorize("hasRole('ROLE_ROOT')")配置 如果都配置 我猜应该先校验数据库配置 再校验注解 推荐使用注解 role resouce role_resouce  需要配置url 并且role名字固定ROLE_* 侵入性太强
+    @ApiOperation(value = "用户列表",tags = "用户列表接口")
     @GetMapping("/list")
     @ResponseBody
     //@PreAuthorize("hasRole('ROLE_ROOT')")
-    public List<User> getUserList() {
+    public Result getUserList() {
         List<User> userList = userMapper.selectAll();
-        return userList;
+        return Result.success(userList);
     }
 
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="userName",value="用户名",dataType="string", paramType = "write",example="test", required = true),
+            @ApiImplicitParam(name="password",value="用户密码",dataType="string", paramType = "write",required = true),
+            @ApiImplicitParam(name="nickName",value="用户昵称",dataType="string", paramType = "write",required = true)
+    })
+    @ApiOperation(value = "添加用户",tags = "用户添加接口")
     @GetMapping ("/add")
     @ResponseBody
     public Result addUser(String userName, String password, String nickName, HttpServletRequest request) {
